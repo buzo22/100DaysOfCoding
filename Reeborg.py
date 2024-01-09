3d666034e0f4c4f7def913efa23f409f60e18bdd
buzo22 <111765508+buzo22@users.noreply.github.com>
2024-01-09 23:43:53 +0100

    Create Reeborg GAme

---
+++ Reeborg.py
@@ -0,0 +1,19 @@
+from robot_simulation import front_is_clear, turn_left, move, right_is_clear, at_goal
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
