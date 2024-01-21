fa40566fa1f9448e56cedb3efac71183789bb133
chibuzo-akana <111765508+buzo22@users.noreply.github.com>
2024-01-21 22:46:12 +0100

    Automated Coffee Dispenser.

--- main.py
+++ main.py
@@ -1,39 +1,85 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',  
-            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 
-            'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',  
-            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 
-            'v', 'w', 'x', 'y', 'z']
+menu = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
-def caesar(start_text, shift_amount, cipher_direction):
-    end_text = ""
-    if cipher_direction == "decode":
-        shift_amount *= -1
-    for char in start_text:
-        if char in alphabet:
-            position = alphabet.index(char)
-            new_position = position + shift_amount
-            end_text += alphabet[new_position]
-        else:
-            end_text += char
-    print(f"Here's the {cipher_dictionary}d result:{end_text}")
-
-from art import logo
-print(logo)
-
-should_end = False
-while not should_end:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-    text = input("Type your message:").lower()
-    shift =  int(input("Type the shift number:\n"))
-
-    shift = shift % 26
-
-    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-
-    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
-
-    if restart == "no":
-        should_end = True
-        print("Goodbye")
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
 
 
+def is_resource_sufficient(order_ingredients):
+    """Returns True when the order can be made, False if ingredients are insufficient."""
+    for item in order_ingredients:
+        if order_ingredients[item] > resources[item]:
+            print(f"Sorry, there is not enough {item}.")
+            return False
+    return True
+
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+def is_transaction_successful(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if money is insufficient."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry, that's not enough money. Money refunded.")
+        return False
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}. Enjoy!")
+
+is_on = True
+
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = menu[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
